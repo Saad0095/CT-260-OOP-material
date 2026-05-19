8d4d48b7db5874a23e9a2d8be565b07caa140bcf
Saad Bin Khalid <saadbinkhalid1895@gmail.com>
2026-05-19 12:18:48 +0500

    made some changes

--- Class codes OOPs/Final lab prep/PP4_1.cpp
+++ Class codes OOPs/Final lab prep/PP4_1.cpp
@@ -1,196 +1,110 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-// Forward Declaration
-template <typename T>
-class Matrix;
+template <typename T> class Matrix;
 
-template <typename T>
-T maxElement(Matrix<T>&);
+template <typename T> T maxElement(Matrix<T> &);
 
-// Custom Exception
 class MatrixException {
 private:
-    string message;
+  string msg;
 
 public:
-    MatrixException(string msg) {
-        message = msg;
-    }
-
-    string what() {
-        return message;
-    }
+  MatrixException(string m) : msg(m) {};
+  string what() { return msg; }
 };
 
-// Template Matrix Class
-template <typename T>
-class Matrix {
+template <typename T> class Matrix {
 private:
-    vector<vector<T>> data;
-    int rows, cols;
+  vector<vector<T>> mat;
+  int rows, cols;
 
 public:
-
-    // Constructor
-    Matrix(int r, int c) {
-
-        if (r <= 0 || c <= 0) {
-            throw MatrixException("Invalid matrix dimensions.");
-        }
-
-        rows = r;
-        cols = c;
-
-        data.resize(rows, vector<T>(cols));
+  Matrix(int r, int c) {
+    if (r <= 0 || c <= 0) {
+      throw MatrixException("Inavlid mat size");
     }
-
-    // Safe operator[]
-    vector<T>& operator[](int index) {
-
-        if (index < 0 || index >= rows) {
-            throw MatrixException("Row index out of bounds.");
-        }
-
-        return data[index];
+    rows = r;
+    cols = c;
+    mat.resize(rows, vector<T>(cols));
+  }
+  vector<T> &operator[](int i) {
+    if (i < 0 || i > rows) {
+      throw MatrixException("Invalid index");
     }
-
-    // Matrix Addition
-    Matrix operator+(Matrix &other) {
-
-        if (rows != other.rows || cols != other.cols) {
-            throw MatrixException("Matrix dimensions must match for addition.");
-        }
-
-        Matrix result(rows, cols);
-
-        for (int i = 0; i < rows; i++) {
-
-            for (int j = 0; j < cols; j++) {
-
-                result.data[i][j] =
-                    data[i][j] + other.data[i][j];
-            }
-        }
-
-        return result;
+    return mat[i];
+  }
+  void sortCols() {
+    for (int j = 0; j < cols; j++) {
+      vector<T> temp;
+      for (int i = 0; i < rows; i++) {
+        temp.push_back(mat[i][j]);
+      }
+      sort(temp.begin(), temp.end());
+      for (int i = 0; i < rows; i++) {
+        mat[i][j] = temp[i];
+      }
     }
+  }
 
-    // Sort Each Column
-    void sortColumns() {
-
-        for (int j = 0; j < cols; j++) {
-
-            vector<T> temp;
-
-            // Copy column
-            for (int i = 0; i < rows; i++) {
-                temp.push_back(data[i][j]);
-            }
-
-            // Sort column
-            sort(temp.begin(), temp.end());
-
-            // Put back
-            for (int i = 0; i < rows; i++) {
-                data[i][j] = temp[i];
-            }
-        }
+  void sortRows() {
+    for (int i = 0; i < rows; i++) {
+      sort(mat[i].begin(), mat[i].end());
     }
+  }
+  void display() {
 
-    // Display Matrix
-    void display() {
+    for (int i = 0; i < rows; i++) {
 
-        for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
 
-            for (int j = 0; j < cols; j++) {
+        cout << mat[i][j] << " ";
+      }
 
-                cout << data[i][j] << " ";
-            }
-
-            cout << endl;
-        }
+      cout << endl;
     }
-
-    // Friend Function
-    friend T maxElement<T>(Matrix<T>&);
+  }
+  friend T maxElement<T>(Matrix<T> &);
 };
 
-// Friend Function Definition
-template <typename T>
-T maxElement(Matrix<T>& m) {
-
-    T maxVal = m.data[0][0];
-
-    for (int i = 0; i < m.rows; i++) {
-
-        for (int j = 0; j < m.cols; j++) {
-
-            if (m.data[i][j] > maxVal) {
-                maxVal = m.data[i][j];
-            }
-        }
+template <typename T> T maxElement(Matrix<T> &obj) {
+  int max = obj.mat[0][0];
+  for (int i = 0; i < obj.rows; i++) {
+    for (int j = 0; j < obj.cols; j++) {
+      if (max < obj.mat[i][j]) {
+        max = obj.mat[i][j];
+      }
     }
+  }
+  return max;
+};
 
-    return maxVal;
-}
-
-// Main Function
 int main() {
+  try {
+    Matrix<int> A(2, 2);
+    Matrix<int> B(2, 2);
 
-    try {
+    A[0][0] = 10;
+    A[0][1] = 5;
+    A[1][0] = 9;
+    A[1][1] = 8;
 
-        Matrix<int> m1(2, 2);
-        Matrix<int> m2(2, 2);
+    B[0][0] = 160;
+    B[0][1] = 65;
+    B[1][0] = 69;
+    B[1][1] = 68;
 
-        // Input values
-        m1[0][0] = 1;
-        m1[0][1] = 4;
-        m1[1][0] = 3;
-        m1[1][1] = 2;
+    cout << "Matrix A: " << endl;
+    A.display();
 
-        m2[0][0] = 5;
-        m2[0][1] = 6;
-        m2[1][0] = 7;
-        m2[1][1] = 8;
-
-        cout << "Matrix 1:\n";
-        m1.display();
-
-        cout << "\nMatrix 2:\n";
-        m2.display();
-
-        // Matrix Addition
-        Matrix<int> sum = m1 + m2;
-
-        cout << "\nSum Matrix:\n";
-        sum.display();
-
-        // Max Element
-        cout << "\nMaximum Element in Matrix 1: "
-             << maxElement(m1) << endl;
-        cout << "\nMaximum Element in Matrix 2: "
-             << maxElement(m2) << endl;
-
-        // Sort Columns
-        m1.sortColumns();
-
-        cout << "\nMatrix 1 After Column Sorting:\n";
-        m1.display();
-
-        // Exception Test
-        cout << m1[5][0];
-
-    }
-
-    catch (MatrixException &e) {
-
-        cout << "\nException Caught: "
-             << e.what() << endl;
-    }
-
-    return 0;
+    cout << "Max elementin matrix A" << maxElement(A) << endl;
+    A.sortCols();
+    cout << "\nMatrix A After Column Sorting:\n";
+    A.display();
+    A.sortRows();
+    cout << "\nMatrix A After Row Sorting:\n";
+    A.display();
+  } catch (MatrixException &e) {
+    cout << e.what() << '\n';
+  }
 }
